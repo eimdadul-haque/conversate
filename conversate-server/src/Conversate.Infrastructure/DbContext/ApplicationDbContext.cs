d4c3c9eee665e36810d92a90f661922387c7187c
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-05-15 03:05:32 +0600

    db context problem fix

---
+++ conversate-server/src/Conversate.Infrastructure/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Conversate.Domain.ApplicationUsers;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Conversate.Infrastructure.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
+
