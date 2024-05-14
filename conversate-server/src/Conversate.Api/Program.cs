fbe7385291854f708fafeb4ea3a22e2aef11976a
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-05-15 03:08:45 +0600

    auth setup in program file

--- conversate-server/src/Conversate.Api/Program.cs
+++ conversate-server/src/Conversate.Api/Program.cs
@@ -1,7 +1,15 @@
 
+using Conversate.Application.Accounts;
 using Conversate.Application.Hubs.MessageHub;
+using Conversate.Domain.ApplicationUsers;
 using Conversate.Infrastructure;
-using Microsoft.EntityFrameworkCore;    
+using Conversate.Infrastructure.DbContext;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +19,65 @@ builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredUniqueChars = 0;
+    options.Password.RequiredLength = 4;
+});
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder
+    .Services
+    .AddDbContext<ApplicationDbContext>(options => options
+    .UseSqlServer(builder.Configuration
+    .GetConnectionString("DefaultConnection")));
+
+builder
+    .Services
+    .AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(option =>
+{
+    option.SaveToken = true;
+    option.RequireHttpsMetadata = false;
+
+    option.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:IssuerSigningKey"]))
+    };
+
+    //option.Events = new JwtBearerEvents()
+    //{
+    //    OnMessageReceived = (context) =>
+    //    {
+    //        var access_token = context.Request.Query["access_token"];
+    //        var path = context.HttpContext.Request.Path;
+
+    //        if(!string.IsNullOrEmpty(access_token) && (path.StartsWithSegments("/message")))
+    //            context.Token = access_token;
+
+    //        return Task.CompletedTask;
+    //    }
+    //};
+});
+
+builder.Services.AddTransient<IAccount, Account>();
 
 builder.Services.AddCors(option =>
 {
@@ -41,7 +105,7 @@ app.UseSwaggerUI(options =>
 app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapHub<MessageHub>("/message"); 
+    endpoints.MapHub<MessageHub>("/message");
 });
 
 app.MapControllers();
