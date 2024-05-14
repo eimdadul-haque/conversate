604fb23ca19156ee8324e66efb71c448344fb96d
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-05-15 03:05:53 +0600

    jwt token service added

---
+++ conversate-server/src/Conversate.Application/Accounts/Account.cs
@@ -0,0 +1,81 @@
+﻿using Conversate.Domain.ApplicationUsers;
+using Conversate.Shared.Account.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversate.Application.Accounts
+{
+    public class Account : IAccount
+    {
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public Account(
+            IConfiguration configuration,
+            SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+            _signInManager = signInManager;
+        }
+
+        public async Task<bool> SignIn(SignUpDto input)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                Name = input.Name,
+                Email = input.Email,
+                UserName = input.Username,
+                PhoneNumber = input.PhoneNumber
+            };
+
+            var result = await _userManager
+                .CreateAsync(applicationUser, input.Password);
+
+            return result.Succeeded;   
+        }
+
+        public async Task<string> Login(LoginDto input)
+        {
+            var result = await _signInManager
+                .PasswordSignInAsync(input.Email, input.Password, input.IsPersistent, false);
+            
+            if (result.Succeeded)
+               return GenerateJwtToken(input.Email);
+            else 
+                return string.Empty;
+        }
+
+        private string GenerateJwtToken(string email)
+        {
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII
+                .GetBytes(_configuration["JWT:IssuerSigningKey"]));
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:Issuer"],
+                audience: _configuration["JWT:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return jwtSecurityTokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ conversate-server/src/Conversate.Application/Accounts/IAccount.cs
@@ -0,0 +1,15 @@
+﻿using Conversate.Shared.Account.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversate.Application.Accounts
+{
+    public interface IAccount
+    {
+        Task<string> Login(LoginDto input);
+        Task<bool> SignIn(SignUpDto input);
+    }
+}
